e0e50cbf666df0d7e9276caa96c0d48abfc27d00
Soumajith Das <91211431+Soumajith@users.noreply.github.com>
2023-07-13 23:39:57 +0530

    Added Cloudinary API

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,1 +1,14 @@
-const cloudinary = require("cloudinary");
+const cloudinary = require("cloudinary").v2;
+require("dotenv").config();
+
+exports.cloudinaryConnect = () => {
+  try {
+    cloudinary.config({
+      cloud_name: process.env.CLOUDINARY_NAME,
+      api_key: process.env.CLOUDINARY_KEY,
+      api_secret: process.env.CLOUDINARY_SECRET,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
