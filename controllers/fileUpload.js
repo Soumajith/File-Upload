e602620aba9d442ba3b8a3ce3af996ab4e5b4098
Soumajith Das <91211431+Soumajith@users.noreply.github.com>
2023-07-15 00:24:47 +0530

    Added Video file upload and reduced image file upload API

--- controllers/fileUpload.js
+++ controllers/fileUpload.js
@@ -1,5 +1,5 @@
 const File = require("../models/file");
-const cloudinary = require("cloudinary");
+const cloudinary = require("cloudinary").v2;
 
 exports.localFileUpload = async (request, response) => {
   try {
@@ -34,11 +34,13 @@ const checkFileType = (fileType, selectedTypes) => {
   return selectedTypes.includes(fileType);
 };
 
-const cloudinaryUpload = async (file, folder) => {
-  const filePathOnCloudinary = folder + "/" + file.tempFilePath;
-  return await cloudinary.uploader.upload(file.tempFilePath, {
-    public_id: filePathOnCloudinary,
-  });
+const cloudinaryUpload = async (file, folder, quality) => {
+  const option = { folder };
+  if (quality) {
+    option.quality = quality;
+  }
+  option.resource_type = "auto";
+  return await cloudinary.uploader.upload(file.tempFilePath, option);
 };
 
 exports.imageUpload = async (request, response) => {
@@ -59,6 +61,86 @@ exports.imageUpload = async (request, response) => {
 
     // cloudinary upload
     const res = await cloudinaryUpload(file, "Soumajith");
+    console.log(res);
+
+    // add in the database
+    const flleData = await File.create({
+      name,
+      email,
+      imageUrl: res.secure_url,
+      tags,
+    });
+    return response.status(200).json({
+      success: true,
+      message: "File uploaded successfully",
+      imageUrl: res.secure_url,
+    });
+  } catch (err) {
+    console.log(err);
+    return response.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
+
+exports.videoUpload = async (request, response) => {
+  try {
+    const { name, email, tags } = request.body;
+    const file = request.files.file;
+    const fileType = file.split(".")[1];
+    const supportedType = ["mp4", "mov", "gif"];
+
+    //validation
+    if (!checkFileType(fileType, supportedType))
+      return response.status(400).json({
+        success: false,
+        message: "File not supported",
+      });
+
+    // upload to cloudinary
+    const res = await cloudinaryUpload(file, "soumajith");
+    console.log(res);
+
+    // add it to the database
+    const fileData = await File.create({
+      name,
+      email,
+      imageUrl: res.secure_url,
+      tags,
+    });
+
+    response.status(200).json({
+      success: true,
+      message: "Video upload successfully",
+    });
+  } catch (err) {
+    console.log(err);
+    response.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
+
+exports.imageReducedUpload = async (request, response) => {
+  try {
+    // fetch
+    const { name, tags, email } = request.body;
+    const file = request.files.imageFile;
+    const selectedTypes = ["png", "jpeg", "jpg"];
+    const fileType = file.name.split(".")[1];
+
+    // validation
+    if (!checkFileType(fileType, selectedTypes)) {
+      return response.status(400).json({
+        success: false,
+        message: "File type not supported",
+      });
+    }
+
+    // cloudinary upload
+    const res = await cloudinaryUpload(file, "Soumajith", 10);
     console.log(res);
 
     // add in the database
--- routes/FileUpload.js
+++ routes/FileUpload.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const { localFileUpload, imageUpload } = require("../controllers/fileUpload");
+const {
+  localFileUpload,
+  imageUpload,
+  imageReducedUpload,
+} = require("../controllers/fileUpload");
 
 router.post("/localfile", localFileUpload);
 router.post("/imageFile", imageUpload);
+router.post("/imageReducedFile", imageReducedUpload);
+
 module.exports = router;
