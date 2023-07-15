39a857fc777e1261774517f25c642b3a670cc5a7
Soumajith Das <91211431+Soumajith@users.noreply.github.com>
2023-07-15 12:14:14 +0530

    Added Mailing service

--- models/file.js
+++ models/file.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
 
 const FileSchema = mongoose.Schema({
   name: {
@@ -15,6 +17,26 @@ const FileSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+});
+
+FileSchema.post("save", async (doc) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+      },
+    });
+    const info = await transporter.sendMail({
+      from: `Cloudidas`,
+      to: doc.email,
+      subject: "New File Upload in Cloudinary",
+      html: `<h1>File Upload</h1><p>Click here to view <a href="${doc.imageUrl}">${doc.imageUrl}</a></p>`,
+    });
+  } catch (err) {
+    console.log("Error", err);
+  }
 });
 
 module.exports = mongoose.model("File", FileSchema);
